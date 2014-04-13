b3f0500dd6497ff6138928b0c09f82a59958d026
sreeramck <nwrbckdwn@gmail.com>
2014-04-13 09:57:58 +0530

    Kick start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirbnbClone::Application.config.secret_token = "c178b05958e8e78f247b2cc6bdf957a784533e95b31e369e59e05f4ee3dbe94a2409b7d1b0f0756c15d07f189dce26ecb4bd13d13945180730ce1d0d5e125779"
